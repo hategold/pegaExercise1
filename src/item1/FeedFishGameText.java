888bfcb2f5f4b13df2f6a250487d15f27d31f98b
tim2_wang <tim2_wang@pegatroncorp.com>
2016-07-25 15:21:14 +0800

    encapture some method from class to inner class

--- src/item1/FeedFishGameText.java
+++ src/item1/FeedFishGameText.java
@@ -67,7 +67,7 @@ public class FeedFishGameText {
 
 	public void buildFishList() {
 		fishList.add(new Guppy("Guppy", 8, AbstractFish.GenderEnum.FEMALE, 100, Guppy.PatternName.COBRA));
-		fishList.add(new RedNeon("RedNeon", 3, AbstractFish.GenderEnum.MALE, 100)); //fish builder 拉出來  method chain);
+		fishList.add(new RedNeon("RedNeon", 3, AbstractFish.GenderEnum.MALE, 100));
 		fishList.add(new Wawa("Wawa", 5, AbstractFish.GenderEnum.MALE, 80));
 
 		//		Fish f2 = new RedNeon("RedNeon", 3, Fish.GenderEnum.MALE, 100);//fish builder 拉出來  method chain
@@ -87,31 +87,6 @@ public class FeedFishGameText {
 		System.out.println("遊戲開始");
 	}
 
-	public void checkWater() {
-		System.out.println(String.valueOf(aq.getNo2()));
-	}
-
-	public String getUserInput(String msg2User) {
-
-		String userCmd = "";
-		System.out.println(msg2User);
-
-		try {
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			userCmd = br.readLine();
-
-			if (userCmd.length() == 0) {
-				return "";
-			}
-
-		} catch (IOException | NumberFormatException e) {
-			System.out.println("Exception: " + e);
-		}
-
-		return userCmd;
-	}
-
 	public int getFfood() {
 		return ffood;
 	}
@@ -124,53 +99,11 @@ public class FeedFishGameText {
 		this.ffood = ffood;
 
 	}
-
-	private void checkGameStatus(List<AbstractFish> fishList) {//改list & 這邊不需要處理extend
-		System.out.println("現在魚缸中的有害物質佔這麼多:" + aq.getNo2());
-		System.out.println("現在魚缸中的殘餘飼料有:" + ffood);
-		for (AbstractFish fish : fishList) {
-			System.out.println(fish.getName() + "的健康度是" + String.valueOf(fish.getHealthDegree()));
-		}
-	}
-
 	private boolean checkFishAlive(AbstractFish fish) {
 		if (fish.getHealthDegree() < 0) {
 			return false;
 		}
 		return true;
-	}
-
-	private void cleanWaterFood() {
-		synchronized (this) {
-			aq.refleshWater();
-			ffood = (int) (ffood * 0.8);
-		}
-	}
-
-	private void userPutFood() {
-
-		String msg = "要加入多少飼料呢? 輸入非數字則取消動作";
-		try {
-			int userCmd = Integer.valueOf(getUserInput(msg));
-			synchronized (this) {
-				ffood += userCmd;
-			}
-			System.out.printf("投入%d顆飼料。\n", userCmd);
-
-		} catch (NumberFormatException e) {
-			System.out.println("飼料只能輸入數字，回到動作選單");
-		}
-	}//可能會想取消輸入
-
-	private UserGameCmd getUserGameCmd() {
-		try {
-			String msg = "你可以進行三個動作 :1.確認魚隻/環境狀態  2.加飼料 3.換水";
-			int inputCmd = Integer.valueOf(getUserInput(msg));
-
-			return UserGameCmd.valueOf(inputCmd);
-		} catch (NumberFormatException e) {
-			return UserGameCmd.NONE;
-		}
 	}
 
 	class UserHandler implements Runnable {
@@ -199,6 +132,73 @@ public class FeedFishGameText {
 				}
 			}
 		}
+
+		private void checkGameStatus(List<AbstractFish> fishList) {//改list & 這邊不需要處理extend
+			System.out.println("現在魚缸中的有害物質佔這麼多:" + aq.getNo2());
+			System.out.println("現在魚缸中的殘餘飼料有:" + ffood);
+			for (AbstractFish fish : fishList) {
+				System.out.println(fish.getName() + "的健康度是" + String.valueOf(fish.getHealthDegree()));
+			}
+		}
+		
+		private void cleanWaterFood() {
+			synchronized (this) {
+				aq.refleshWater();
+				ffood = (int) (ffood * 0.8);
+			}
+		}
+
+		private void userPutFood() {
+
+			String msg = "要加入多少飼料呢? 輸入非數字則取消動作";
+			try {
+				int userCmd = Integer.valueOf(getUserInput(msg));
+				synchronized (this) {
+					ffood += userCmd;
+				}
+				System.out.printf("投入%d顆飼料。\n", userCmd);
+
+			} catch (NumberFormatException e) {
+				System.out.println("飼料只能輸入數字，回到動作選單");
+			}
+		}//可能會想取消輸入
+
+		private UserGameCmd getUserGameCmd() {
+			try {
+				String msg = "你可以進行三個動作 :1.確認魚隻/環境狀態  2.加飼料 3.換水";
+				int inputCmd = Integer.valueOf(getUserInput(msg));
+
+				return UserGameCmd.valueOf(inputCmd);
+			} catch (NumberFormatException e) {
+				return UserGameCmd.NONE;
+			}
+		}
+
+		public void checkWater() {
+			System.out.println(String.valueOf(aq.getNo2()));
+		}
+
+		public String getUserInput(String msg2User) {
+
+			String userCmd = "";
+			System.out.println(msg2User);
+
+			try {
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				userCmd = br.readLine();
+
+				if (userCmd.length() == 0) {
+					return "";
+				}
+
+			} catch (IOException | NumberFormatException e) {
+				System.out.println("Exception: " + e);
+			}
+
+			return userCmd;
+		}
+
 	}
 
 	class FishActionHandler implements Runnable {
